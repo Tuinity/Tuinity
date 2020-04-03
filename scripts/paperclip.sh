1f470f529bfc2965f3bfc024ae4ce6f763fea40c
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2020-04-03 16:34:00 +0800

    paperclip.sh: remove unnecessary lines

--- scripts/paperclip.sh
+++ scripts/paperclip.sh
@@ -3,7 +3,6 @@
 # Copied from https://github.com/PaperMC/Paper/blob/d54ce6c17fb7a35238d6b9f734d30a4289886773/scripts/paperclip.sh
 # License from Paper applies to this file
 
-(
 set -e
 basedir="$(cd "$1" && pwd -P)"
 workdir="$basedir/Paper/work"
@@ -22,4 +21,3 @@ echo ""
 echo ""
 echo "Build success!"
 echo "Copied final jar to $(cd "$basedir" && pwd -P)/tuinity-paperclip.jar"
-) || exit 1
