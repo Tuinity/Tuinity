2e385332d9586d5aa93f277da4a7b6be1ca5b3eb
Spottedleaf <spottedleaf@spottedleaf.dev>
2020-07-05 01:13:30 -0700

    Updated Upstream (Paper)
    Upstream has released updates that appears to apply and compile correctly
    
    Paper Changes:
    97f920bf1 Fix suggest-player-names-when-null-tab-completions - Fixes #3803
    166e52b57 [CI-SKIP] [Auto] Rebuild Patches
    5a17ba07b Implements #3761 - Add entity liquid API (#3762)
    e0cae289c Fix anvils bug - Fixes #3802
    4793f774c Move bedrock config under unsupported, add comments to these configs
    adf1de586 Allow delegation to vanilla chunk gen
    40ace3f7b Allow delegation to vanilla chunk gen
    178a6e502 Add PrepareResultEvent (#3776)
    57697cd51 Report proxy onlinde mode to bstats as online (#3093)
    6579ce051 Fix #3701 - Loottables erasing on viewing
    030da7b89 [CI-SKIP] [Auto] Rebuild Patches
    d43def795 Incorrect spawn reason for monsters from spawner - Fixes #3708 (#3764)
    97b59df9c [Auto] Updated Upstream (CraftBukkit)
    0543f051a [CI-SKIP] [Auto] Rebuild Patches
    f3cd94c42 Remove streams from classes related to villager gossip (#3748)
    b49104db6 add EntityTargetLivingEntityEvent for new 1.16 mobs (#3782)
    4faf9703b Fix /plugins list not alphabetical to players (#3790)
    7e03e44ea [CI-SKIP] [Auto] Rebuild Patches
    8bb002728 Update itemstack legacy name and lore (#3741)
    6c87b6a00 [Auto] Updated Upstream (CraftBukkit)
    ad52d2118 [Auto] Updated Upstream (CraftBukkit)
    0dcce21fe Merge remote-tracking branch 'origin/ver/1.16'
    6ff90880f Merge branch 'ver/1.16'
    2248fffcd Clean up duplicated GameProfile Properties
    49491f32d Fix Player Profile textures being duplicated - Fixes #3667
    3fc989992 [1.15] Fix MobGoals#getAllGoals not actually returning all goals (#3671)
    1d1c0561f Manually inline PooledBlockPosition#d(int, int, int)
    5fc45f4db Revert recent changes around player skulls using user cache
    a8ffecc74 Fix NPE and uuid lookup for player skulls

--- scripts/upstream.sh
+++ scripts/upstream.sh
@@ -13,7 +13,7 @@ git submodule update --init --recursive
 if [[ "$1" == up* ]]; then
     (
         cd "$basedir/Paper/"
-        git fetch && git reset --hard origin/ver/1.16
+        git fetch && git reset --hard origin/master
         cd ../
         git add Paper
     )
