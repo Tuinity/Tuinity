9a6fb96f4f0f01fac658a0ca9a5f265b44dd1619
Spottedleaf <spottedleaf@spottedleaf.dev>
2020-07-05 01:20:07 -0700

    Change paper target branch to ver/1.15.2

--- scripts/upstream.sh
+++ scripts/upstream.sh
@@ -13,7 +13,7 @@ git submodule update --init --recursive
 if [[ "$1" == up* ]]; then
     (
         cd "$basedir/Paper/"
-        git fetch && git reset --hard origin/master
+        git fetch && git reset --hard origin/ver/1.15.2
         cd ../
         git add Paper
     )
