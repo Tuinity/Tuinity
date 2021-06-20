85ed70e9d6e89ae34b59c9e626331cee1d317040
Spottedleaf <Spottedleaf@users.noreply.github.com>
2021-06-19 22:03:46 -0700

    Paperweight 1.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.papermc.paperweight.util.Constants
 plugins {
     java
     id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.1.3-SNAPSHOT"
+    id("io.papermc.paperweight.patcher") version "1.1.3"
 }
 
 repositories {
